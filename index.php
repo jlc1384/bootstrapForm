ce473126bf92da83e5ebbd5e6625119f48f22a97
Jesus Lago <jlago@pulsartec.com>
2017-10-16 15:35:51 +0200

    Form finished

--- index.php
+++ index.php
@@ -9,143 +9,107 @@
 		<link rel="stylesheet" href="web/lib/bootstrap-3.3.7-dist/css/bootstrap-datetimepicker.min.css" type="text/css" />
 		<link rel="stylesheet" href="web/lib/bootstrap-3.3.7-dist/css/ai.css" type="text/css" />
 		<link rel="stylesheet" href="web/lib/bootstrap-3.3.7-dist/css/bootstrap-multiselect.css" type="text/css" />
+		<link rel="stylesheet" href="web/lib/bootstrap-3.3.7-dist/css/bootstrapValidator.min.css" type="text/css" />
 		<link rel="shortcut icon" type="image/png" href="bootstrap-3.3.7-dist/img/indIcon.png"/>
 	</head>
 	<body>
 	 	<div class="container">
 		    <div class="signup-form-container">
-		    
-		         <!-- form start -->
-		         <form role="form" id="register-form" autocomplete="off">
-		         
-				<div class="form-header">
-					<h3 class="form-title"><i class="fa fa-user"></i>Auto Increase Check</h3>
- 				</div>
-		         
-				<div class="form-body">
+				<form role="form" id="aiForm" name="aiForm" autocomplete="off">
+					<div class="form-header">
+						<h3 class="form-title"><i class="fa fa-user"></i>Auto Increase Check</h3>
+	 				</div>
+			         
+					<div class="form-body">
 				          
-				<div class="form-group">
-					<div class="input-group">
-						<div class="input-group-addon">
-							<span class="glyphicon glyphicon-th-large"></span>
+						<div class="form-group">
+							<div class="input-group">
+								<div class="input-group-addon">
+									<span class="glyphicon glyphicon-th-large"></span>
+								</div>
+								<select name="dealType" id="dealType" class="bootstrap-select selectpicker" data-live-search="true" data-width="500px" placeholder="Deal Type">
+									<option value="">Select Deal Type</option>
+									<option value="de1">Base Contract</option>
+									<option value="de2">Add-On Contract</option>
+								</select>
+							</div>
+							<span class="help-block" id="error"></span>
 						</div>
-						<select class="bootstrap-select selectpicker" data-live-search="true" data-width="500px" placeholder="Deal Type">
-							<option value="">Select Deal Type</option>
-							<option value="oda">ODA Base-Variable / ODA Lite</option>
-							<option value="oda2">ODA 2.0</option>
-						</select>
-					</div>
-					<span class="help-block" id="error"></span>
-				</div>
 				        
-				<div class="form-group">
-					<div class="input-group">
-						<div class="input-group-addon">
-							<span class="glyphicon glyphicon-list-alt"></span>
+						<div class="form-group">
+							<div class="input-group">
+								<div class="input-group-addon">
+									<span class="glyphicon glyphicon-list-alt"></span>
+								</div>
+								<select name="funderSel" id="funderSel" class="bootstrap-select selectpicker" data-live-search="true" data-width="500px" placeholder="Funder">
+									<option value="">Select Funder</option>
+									<option value="1">Funder 1</option>
+									<option value="2">Funder 2</option>
+									<option value="3">Funder 3</option>
+									<option value="4">Funder 4</option>
+								</select>
+							</div> 
+							<span class="help-block" id="error"></span>                     
 						</div>
-						<select class="bootstrap-select selectpicker" data-live-search="true" data-width="500px" placeholder="Funder">
-							<option value="">Select Funder</option>
-							<option value="1">Funder 1</option>
-							<option value="2">Funder 2</option>
-							<option value="3">Funder 3</option>
-							<option value="4">Funder 4</option>
-						</select>
-					</div> 
-					<span class="help-block" id="error"></span>                     
-				</div>
 				
-				<div class="form-group">
-					<div class="input-group">
-						<div class="input-group-addon">
-							<span class="glyphicon glyphicon-th-list"></span>
+						<div class="form-group">
+							<div class="input-group">
+								<div class="input-group-addon">
+									<span class="glyphicon glyphicon-th-list"></span>
+								</div>
+								<select class="input-group-addon" name="companyAI" id="companyAI" data-width="500px" multiple placeholder="Company">
+									<option value="1">Company 1</option>
+									<option value="2">Company 2</option>
+									<option value="3">Company 3</option>
+									<option value="4">Company 4</option>
+									<option value="5">Company 5</option>
+									<option value="6">Company 6</option>
+								</select>
+							</div>
+							<span class="help-block" id="error"></span>
 						</div>
-						<select class="bootstrap-select form-control" name="companyAI" id="companyAI" data-width="500px" multiple placeholder="Company">
-							<option value="1">Company 1</option>
-							<option value="2">Company 2</option>
-							<option value="3">Company 3</option>
-							<option value="4">Company 4</option>
-							<option value="5">Company 5</option>
-							<option value="6">Company 6</option>
-						</select>
+				
+						<div class="row">
+							<div class="form-group col-lg-6">
+				                <div class='input-group date datepickerclass'>
+				                	<span class="input-group-addon">
+				                        <span class="glyphicon glyphicon-calendar"></span>
+				                    </span>
+
+									<input type="text" class="form-control" name="upToDate" id="upToDate" placeholder="Up to Date *" data-fv-date="true" data-fv-date-format="YYYY-MM-DD" data-fv-date-message="Please fill out a valid date" />
+
+				                </div>
+			        		</div>
+							
+							<div class="form-group col-lg-6">
+								<div class="grcheckbox">
+							        <div class="grcheckbox-default">
+							            <input type="checkbox" name="inclAllContr" id="inclAllContr" checked/>
+							            <label for="inclAllContr">Include all Contracts (Not consider quarter dates) </label>
+							        </div>
+							    </div>
+							</div>
+						</div>
+
+						<div class="form-footer">
+						     <button type="submit" class="btn btn-success">
+						     	<span class="glyphicon glyphicon-ok"></span> Accept
+						     </button>
+						     <button type="submit" class="btn btn-danger">
+						     	<span class="glyphicon glyphicon-remove"></span> Cancel
+						     </button>
+						</div>
 					</div>
-					<span class="help-block" id="error"></span>
-				</div>
-
-				<div class="form-group">
-	                <div class='input-group date' id='upToDate'>
-	                	<span class="input-group-addon">
-	                        <span class="glyphicon glyphicon-calendar"></span>
-	                    </span>
-	                    <input type='text' class="form-control" placeholder="Up to Date *"/>
-	                </div>
-        		</div>
-				
-				<div class="form-group">
-					<div class="grcheckbox">
-				        <div class="grcheckbox-default">
-				            <input type="checkbox" name="checkbox" id="checkbox1" checked/>
-				            <label for="checkbox1">Include all Contracts (Not consider quarter dates) </label>
-				        </div>
-				    </div>
-				</div>
-
-
-				<!-- <div class="form-group">
-					<div class="input-group">
-						<div class="input-group-addon">
-							<span class="glyphicon glyphicon-calendar"></span>
-						</div>
-						<div class='input-group date' id='upToDate'>
-		                    <input type='text' id="upToDate"  class="form-control" placeholder="Up to Date"/>
-		                </div>
-					</div> 
-					<span class="help-block" id="error"></span>                     
-				</div> -->
-
-				  <!-- <div class="row">
-				            
-				       <div class="form-group col-lg-6">
-				            <div class="input-group">
-				            <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></div>
-				            <input name="password" id="password" type="password" class="form-control" placeholder="Password">
-				            </div>  
-				            <span class="help-block" id="error"></span>                    
-				       </div>
-				                
-				       <div class="form-group col-lg-6">
-				            <div class="input-group">
-				            <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></div>
-				            <input name="cpassword" type="password" class="form-control" placeholder="Retype Password">
-				            </div>  
-				            <span class="help-block" id="error"></span>                    
-				       </div>
-				                
-				 </div> -->
-				            
-				            
-				</div>
-
-				<div class="form-footer">
-				     <button type="submit" class="btn btn-success">
-				     	<span class="glyphicon glyphicon-ok"></span> Accept
-				     </button>
-				     <button type="submit" class="btn btn-danger">
-				     	<span class="glyphicon glyphicon-remove"></span> Accept
-				     </button>
-				</div>
-
-
 				</form>
-
 				</div>
-
-		 </div>
+ 			</div>
 		    
 		<script src="web/lib/jquery/jquery-3.2.1.min.js"></script>
 		<script src="web/lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 		<script src="web/lib/bootstrap-3.3.7-dist/node_modules/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
 		<script src="web/lib/bootstrap-3.3.7-dist/js/bootstrap-datetimepicker.min.js"></script>
 		<script src="web/lib/bootstrap-3.3.7-dist/js/bootstrap-multiselect.js"></script>
+		<script src="web/lib/bootstrap-3.3.7-dist/js/bootstrapValidator.min.js"></script>
 		<script src="web/lib/jquery/aiForm.js"></script>
 	</body>
 </html>
--- web/lib/jquery/aiForm.js
+++ web/lib/jquery/aiForm.js
@@ -1,17 +1,68 @@
 $(document).ready(function() {
 
 	$('#upToDate').datetimepicker({
-		minView : 2,
-		format: 'yyyy-mm-dd', 
-	    onClose: function() {
-	        $('#upToDate').valid();
-	    }
-	});
+		minView : "2",
+        max: "2",
+        minlength: "10",
+		format: 'yyyy-mm-dd',
+        todayBtn: true,
+        todayHighlight: true,
+        autoclose: true
+	}).on('changeDate show', function(e) {
+        // Revalidate the date when user change it
+        $('#aiForm').bootstrapValidator('revalidateField', 'upToDate');
+    });
 
 	$('#companyAI').multiselect({
         enableFiltering: true,
         includeSelectAllOption: true,
-        nonSelectedText: 'Company'
+        nonSelectedText: 'Company',
+        buttonContainer: '<div class="dropdown-toggleJ"/>',
     });
+
+	$('#aiForm').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+        	dealType: {
+                validators: {
+                        notEmpty: {
+                        message: 'Please select a Deal Type'
+                    }
+                }
+            },
+            funderSel: {
+                validators: {
+                        notEmpty: {
+                        message: 'Please select a Funder'
+                    }
+                }
+            },
+
+            companyAI: {
+            	validators: {
+                        notEmpty: {
+                        message: 'Please select one or more companies'
+                    }
+                }
+            },
+
+            upToDate: {
+            	validators: {
+                        notEmpty: {
+                        message: 'Please select the Up to Date'
+                    },
+                    date: {
+                        format: 'YYYY-MM-DD',
+                        message: 'Please select a valid Up to Date using the correct format (yyyy-mm-dd)'
+                    }
+                }
+            }
+        }
+    });
+
 
 });
